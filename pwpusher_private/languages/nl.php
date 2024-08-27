ca62a75117bffa09742d458d906f6956c529c956
Tom Reijnders <reijnders@tor.nl>
2024-08-27 12:47:08 +0200

    Add nl language file

---
+++ pwpusher_private/languages/nl.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Translation Definitions
+ *
+ * Language: English
+ */
+
+$aboutNavLink = 'Over';
+$apacheLogout = 'Sluit uw brwser om deze sessie te beëindigen!';
+$copyToClipboard = 'Kopiëer naar Clipboard';
+$copySuccess = 'Gekopiëerd!';
+$createLink = 'Creëer link';
+$createNavLink = 'Creëer';
+$credentialsCreated = 'Gecreëerd!';
+$databaseErrorGeneric = 'Er ging iets mis met de communicatie met de database.';
+$day = 'dag';
+$days = 'dagen';
+$deleteLink = 'Verwijder link';
+$emailSent = 'Email is verzonden naar ';
+$emailSignature = 'Dank!';
+$emailWarn = 'Deze link bevat gevoelige informatie en zal verwijderd worden als een van de volgende condities bereikt is:';
+$enterSecret = 'Type de gevoelige informatie hieronder in:';
+$enterTime = 'Wat is de geldige tijdslimiet:';
+$timeTooltip = 'De maximale tijd dat de infomratie op de server bewaard blijft.';
+$expiredLink = 'Deze link is niet meer geldig!';
+$giveLink = 'Hier is de link:';
+$hour = 'uur';
+$hours = 'uren';
+$introduction ='Dit formulier kan gebruikt worden om gevoelige informatie naar derden te versturen. Voor de gevoelgige informatie in in het eerste veld, geef vervolgens de tijdslimiet en het maximaal aantal keren dat de link bekeken mag worden. De informatie wordt automatisch verwijderd als een van de condities bereikt is.';
+$linkErased = 'Link is verwijderd.';
+$linkNotErased = 'Link is niet gevonden. Mogelijk is deze al verwijderd.';
+$logoutNavLink = 'Log uit';
+$minute = 'minuut';
+$minutes = 'minuten';
+$noEmail = 'Geen email adres was ingegegevn, dus de email is niet verzonden.';
+$recipientEmailPlaceholder = 'recipient@example.com';
+$recipientEmailTooltip = 'Het email adres waar de informatie neer verzonden moet worden.';
+$recipientNamePlaceholder = 'Naam van de ontvanger';
+$recipientNameTooltip = 'De naam van de persoon waar de informatie naar toe verzonden moet worden.';
+$secret = 'Geheim';
+$secretTooltip = 'Dit is de gevoelige informatie die verzonden moet worden.';
+$sender = 'Verzender';
+$sentCredential = 'Credential from';
+$sharedCredential = 'Het gedeelde wachtwoord:';
+$userNotAuthenticated = 'User not authenticated!';
+$validEmail = 'Email adres is niet geldig!';
+$validName = 'Naam van de ontvanger is niet geldig!';
+$validTimeLimit = 'De tijdlimiet is groter dan het toegestane maximum';
+$validViewLimit = 'Geef een geldig aantal toegestane views!';
+$views = 'views';
+$viewsTooltip = 'Het maixmaal aantal toegstane eren dat de link geopend kan worden voordat de informatie automatisch wordt verwijderd.';
